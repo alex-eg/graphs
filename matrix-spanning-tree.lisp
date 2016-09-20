69a60072562e546c8d75dfe6845498acfcf40f8b
taptap <alexey.e.egorov@gmail.com>
2016-09-20 08:59:09 +0300

    add mst builing functions
    As Kruskal returns a list of weighted edges, we still need to build a
    proper tree, suited for search and traversal.

--- matrix-spanning-tree.lisp
+++ matrix-spanning-tree.lisp
@@ -397,6 +397,54 @@ for this function"
           (push e mst))))
     mst))
 
+
+(defun find-all (item list &key (key #'identity) (test #'=))
+  (let (res)
+    (dolist (e list res)
+      (if (funcall test (funcall key e) item)
+          (push e res)))))
+
+(defun remove-all (item-list list)
+  (reduce (lambda (a e) (remove e a))
+          item-list :initial-value list))
+
+(defun find-item (tree num)
+  "Tree is classic: car is value, cdr is list with all children"
+  (if (= num (car tree))
+      tree
+      (car
+       (remove-if #'null
+                  (mapcar (lambda (subtree) (find-item subtree num))
+                          (cdr tree))))))
+
+(defun build-tree-from-edges (weighted-edges)
+  (let* ((from (mapcar #'weighted-edge-from weighted-edges))
+         (to (mapcar #'weighted-edge-to weighted-edges))
+         (root (apply #'max (append from to)))
+         (edges (copy-list weighted-edges)))
+    (labels ((find-and-delete-all-children (node)
+               (let* ((tos (find-all node edges :key #'weighted-edge-to))
+                      (froms (find-all node edges :key #'weighted-edge-from))
+                      (children (append (mapcar #'weighted-edge-from tos)
+                                        (mapcar #'weighted-edge-to froms))))
+                 (setf edges (remove-all (append tos froms) edges))
+                 children))
+             (build-tree (node)
+               (let ((children (find-and-delete-all-children  (car node))))
+                 (setf (cdr node) (mapcar #'list children))
+                 (mapcar #'build-tree (cdr node)))))
+      (let ((tree (list root)))
+        (build-tree tree)
+        tree))))
+
+(defun find-leaf (tree)
+  (let ((children (cdr tree)))
+    (if (null children)
+        tree
+        (if (null (cdr (car children)))
+            tree
+            (find-leaf (car children))))))
+
 ;;; Pieper algorithm for counting MST in graph
 
 (defun a~-matrix (m n)
